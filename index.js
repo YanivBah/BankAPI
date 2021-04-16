e08ba77b88b5eec8699d0d260d00370d8ad70da7
3agleFF67c1 <ybahalker@gmail.com>
2021-04-16 20:01:19 +0300

    refactor to use queries

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const e = require("express");
 const express = require("express");
 const { newUser, deposit, usersDetails, userDetails, updateCredit, transfer, withdraw } = require("./utils");
 
@@ -10,38 +11,47 @@ app.post("/api/newuser/:passportID", async (req, res) => {
   res.status(status).json(data);
 });
 
-app.put("/api/deposit/:passportID/:cash", async (req, res) => {
-  const { passportID, cash } = req.params;
-  const { status, data } = await deposit(passportID, cash);
-  res.status(status).json(data);
-});
-
-app.put("/api/credit/:passportID/:credit", async (req, res) => {
-  const { passportID, credit } = req.params;
-  const { status, data } = await updateCredit(passportID, credit);
-  res.status(status).json(data);
-});
-
-app.put("/api/transfer/:fromPassportID/:cash/:toPassportID", async (req, res) => {
-  const { fromPassportID, cash, toPassportID } = req.params;
-  const { status, data } = await transfer(fromPassportID, cash, toPassportID);
-  res.status(status).json(data);
-});
-
-app.put("/api/withdraw/:passportID/:cash", async (req, res) => {
-  const { passportID, cash } = req.params;
-  const { status, data } = await withdraw(passportID, cash);
-  res.status(status).json(data);
-});
-
-app.get("/api/users", async (req, res) => {
-  const { status, data } = await usersDetails();
-  res.status(status).json(data);
-});
-
-app.get("/api/user/:passportID", async (req, res) => {
+app.put("/bank/:passportID/query", async (req, res) => {
   const { passportID } = req.params;
-  const { status, data } = await userDetails(passportID);
+  const { type } = req.query;
+  if (!type) return res.status(400).json({error: 'Missing type query!'});
+  switch (type) {
+    case "deposit": {
+      const { cash } = req.query;
+      const { status, data } = await deposit(passportID, cash);
+      res.status(status).json(data);
+      break;
+    }
+    case "credit": {
+      const { credit } = req.query;
+      const { status, data } = await updateCredit(passportID, credit);
+      res.status(status).json(data);
+      break;
+    }
+    case "transfer": {
+      const { cash, to } = req.query;
+      const { status, data } = await transfer(passportID, cash, to);
+      res.status(status).json(data);
+      break;
+    }
+    case "withdraw": {
+      const { cash } = req.query;
+      const { status, data } = await withdraw(passportID, cash);
+      res.status(status).json(data);
+      break;
+    }
+    default:
+      break;
+  }
+});
+
+app.get("/bank/users", async (req,res) => {
+  const {id} = req.query;
+  if (!id) {
+    const { status, data } = await usersDetails();
+    return res.status(status).json(data);
+  }
+  const { status, data } = await userDetails(id);
   res.status(status).json(data);
 });
 
